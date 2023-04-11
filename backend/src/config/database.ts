9802320ce2e12f870951a6b5e6b8d8538dee263f
Yago Chapiqui <55962643+yagochapiqui@users.noreply.github.com>
2023-04-11 12:23:28 -0300

    Update database.ts

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -6,7 +6,7 @@ module.exports = {
     collate: "utf8mb4_bin"
   },
   dialect: process.env.DB_DIALECT || "postgres",
-  timezone: "-03:00",
+  timezone: "-04:00",
   host: process.env.DB_HOST,
    database: process.env.DB_NAME,
   username: process.env.DB_USER,
